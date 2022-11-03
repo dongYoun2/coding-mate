72ed1042e3178ab7fafe3013db81839386d6237c
dongYoun2 <dlehddbs6425@gmail.com>
2022-11-03 14:00:00 +0900

    add firebase auth

--- app/src/main/java/com/bokchi/mysolelife/MainActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/MainActivity.kt
@@ -3,11 +3,40 @@ package com.bokchi.mysolelife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+        joinBtnClicked.setOnClickListener {
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+        }
+
+        }
     }
 }
