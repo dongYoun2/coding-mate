7b7d56d6135fd5c710d8dc7931ef6b2a0cfa0866
dongYoun2 <dlehddbs6425@gmail.com>
2022-11-04 11:00:00 +0900

    fix signup by using databinding

--- app/src/main/java/com/bokchi/mysolelife/MainActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/MainActivity.kt
@@ -3,6 +3,10 @@ package com.bokchi.mysolelife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.bokchi.mysolelife.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -11,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         auth = Firebase.auth
@@ -18,11 +24,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
         joinBtnClicked.setOnClickListener {
 
-            val email = findViewById<EditText>(R.id.emailArea)
-            val pwd = findViewById<EditText>(R.id.pwdArea)
+            val email =binding.emailArea
+            val pwd = binding.pwdArea
 
             auth.createUserWithEmailAndPassword("test@test.com", "4123")
                 .addOnCompleteListener(this) { task ->
