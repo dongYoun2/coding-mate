9cd66b8ac21b0543761cb1263ceb7fc41d8a6a72
dongYoun2 <dlehddbs6425@gmail.com>
2022-11-11 18:31:00 +0900

    edit login feature & MainActivity -> LoginActivity

--- app/src/main/java/com/bokchi/mysolelife/auth/LoginActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/auth/LoginActivity.kt
@@ -1,11 +1,14 @@
-package com.bokchi.mysolelife
+package com.bokchi.mysolelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bokchi.mysolelife.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
