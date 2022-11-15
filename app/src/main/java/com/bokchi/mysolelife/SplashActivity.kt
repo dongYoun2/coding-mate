3c277b7798b063122129d06b9f84a657a9e5445d
sa03134 <jungwoosohn71@gmail.com>
2022-11-15 13:17:00 +0900

    firebase auth 앞에 빠진 거 추가

--- app/src/main/java/com/bokchi/mysolelife/SplashActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/SplashActivity.kt
@@ -7,6 +7,9 @@ import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import com.bokchi.mysolelife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
 
@@ -17,10 +20,20 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null) {
+            Log.d("SplashActivity", "null")
+
+        } else {
+            Log.d("SplashActivity", "not null")
+        }
+
+
+       Handler().postDelayed({
+           startActivity(Intent(this, IntroActivity::class.java))
+           finish()
+       }, 3000)
 
     }
 }
