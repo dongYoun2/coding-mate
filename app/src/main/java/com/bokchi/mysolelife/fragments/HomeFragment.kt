61607bb4b3ad4dfc0d93caee7b76343ad29605f5
sa03134 <jungwoosohn71@gmail.com>
2022-11-16 14:11:00 +0900

    HomeFragment 작업중 근데 toast 안나옴..

--- app/src/main/java/com/bokchi/mysolelife/fragments/HomeFragment.kt
+++ app/src/main/java/com/bokchi/mysolelife/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.bokchi.mysolelife.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,6 +19,8 @@ import com.bokchi.mysolelife.R
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var binding : FragmentHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,9 +30,16 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
+        Log.d("HomeFragment", "onCreateView")
 
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        binding.tipTap.setOnClickListener {
+            Log.d("HomeFragment", "tipTap")
+            Toast.makeText(requireContext(), "clicked", Toast.LENGTH_LONG).show()
+        }
+
+        return binding.root
 }
+
