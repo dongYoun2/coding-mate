1cf7f0fb212d42e779793bb926b9f6c35aed71bb
dongYoun2 <dlehddbs6425@gmail.com>
2022-11-18 19:33:00 +0900

    firebase signup test MainActivity -> JoinActivitiy

--- app/src/main/java/com/bokchi/mysolelife/auth/JoinActivity.kt
+++ app/src/main/java/com/bokchi/mysolelife/auth/JoinActivity.kt
@@ -2,7 +2,10 @@ package com.bokchi.mysolelife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
 import com.bokchi.mysolelife.R
+import com.google.firebase.auth.FirebaseAuth
 
 class JoinActivity : AppCompatActivity() {
 
@@ -10,6 +13,18 @@ class JoinActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+
+        auth.createUserWithEmailAndPassword("test@test.com", "4123")
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
+                    }
+        }
 
     }
 }
